a7327d17796e93e0565f725d69fb0297530d2456
Marta Paz <pazmarta@gmail.com>
2019-02-08 13:19:39 +0000

    add Test After 2

--- python-tdd/counts.py
+++ python-tdd/counts.py
@@ -1,14 +1,44 @@
-""" test after """
-def count_upper_case(message):
+"""1) test after """
+def count_upper_case1(message):
     count = 0
     for c in message:
         if c.isupper():
             count += 1
     return count
 
+assert count_upper_case1("") == 0, "Empty string"
+assert count_upper_case1("A") == 1, "One upper case"
+assert count_upper_case1("a") == 0, "One lower case"
+assert count_upper_case1("£$%%^") == 0, "Special characters"
+
+print("All the tests passed")
+
+
+"""
+2) Test>
+    1. Modify one of the asserts to make it fail.
+    2. Add more tests until be sure that the code is correct.
+    3. Test the following version of the function and see if it is correct 
+
+def count_upper_case(message):
+    return sum([1 for c in message if c.isupper()])
+"""
+def count_upper_case(message):
+    """
+    Count the number of upper case characters in a given message
+    `message` is the piece of text to be checked
+    
+    Returns the number of uppercase characters in a message
+    """
+    return sum([1 for c in message if c.isupper()])
+
 assert count_upper_case("") == 0, "Empty string"
 assert count_upper_case("A") == 1, "One upper case"
+assert count_upper_case("AA") == 2, "Two upper case"
 assert count_upper_case("a") == 0, "One lower case"
 assert count_upper_case("£$%%^") == 0, "Special characters"
 
-print("All the tests passed")
+# An example of a failed test would be -
+# assert count_upper_case("A") == 0, "One upper case"
+
+print("All the tests passed!")
