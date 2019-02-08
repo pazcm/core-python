f9404704bf2ad123eb3ca55c425d4e5d87731636
Marta Paz <pazmarta@gmail.com>
2019-02-08 14:36:59 +0000

    Test Framework 1

--- python-tdd/byotest.py
+++ python-tdd/byotest.py
@@ -1,17 +1,46 @@
-def number_of_evens(numbers):
-    return 0
+"""
+Test Driven Development
+Build My Own Test Framework
 
+1) Tests that fail the test_not_equal, and test_is_in test methods. 
+2) Verify that the message is correct for the values given.
+"""
 
 def test_are_equal(actual, expected):
+    """
+    Test that two values are equal. Raises AssertionError if both values are
+    not equal.
+    `actual` is the actual value produced
+    `expected` is the value that was supposed to be produced
+    """
     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
 
 
 def test_not_equal(a, b):
+    """
+    Test that two values are not equal. Raises AssertionError if both values
+    are not equal.
+    `a` is the actual value produced
+    `b` is the value that was supposed to be produced
+    """
     assert a != b, "Did not expect {0} but got {1}".format(a, b)
 
 
 def test_is_in(collection, item):
+    """
+    Check to ensure that a given collection contains a given value. Raises
+    AssertionError if `item` is not in `collection`
+    `collection` is the collection to be tested
+    `item` is the item that is being searched for
+    """
     assert item in collection, "{0} does not contain {1}".format(collection, item)
 
 
-test_are_equal(number_of_evens([1,2,3,4,5]), 2)
+# Test to fail the `test_are_equal` function
+# test_are_equal(number_of_evens([1,2,3,4,5]), 2)
+
+# Test to fail the `test_not_equal` function
+# test_not_equal(0, 0)
+
+# Test to fail the `test_is_in` function
+# test_is_in([1], 2)
