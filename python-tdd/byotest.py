d569f496306fc4b9f7e15cbcd14ec0dd6a129f9c
Marta Paz <pazmarta@gmail.com>
2019-02-08 14:39:53 +0000

    Test Framework 2

--- python-tdd/byotest.py
+++ python-tdd/byotest.py
@@ -2,8 +2,9 @@
 Test Driven Development
 Build My Own Test Framework
 
-1) Tests that fail the test_not_equal, and test_is_in test methods. 
-2) Verify that the message is correct for the values given.
+1) test methods for `test_not_in` which tests than an item is not in a collection, 
+and `test_between` which tests that a value is between a lower and upper limit, inclusive.
+
 """
 
 def test_are_equal(actual, expected):
@@ -13,7 +14,8 @@ def test_are_equal(actual, expected):
     `actual` is the actual value produced
     `expected` is the value that was supposed to be produced
     """
-    assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
+    assert expected == actual, "Expected {0}, got {1}".format(
+        expected, actual)
 
 
 def test_not_equal(a, b):
@@ -33,7 +35,27 @@ def test_is_in(collection, item):
     `collection` is the collection to be tested
     `item` is the item that is being searched for
     """
-    assert item in collection, "{0} does not contain {1}".format(collection, item)
+    assert item in collection, "{0} does not contain {1}".format(
+        collection, item)
+
+
+def test_not_in(collection, item):
+    """
+    Check to ensure that a given collection does not contain a given value.
+    Raises AssertionError if the `item` is found in `collection`
+    `collection` is the collection in question
+    `item` is the thing that we want to check for
+    """
+    assert item not in collection, "{0} is not in {1}".format(
+        item, collection)
+
+
+def test_between(upper_limit, lower_limit, actual):
+    """
+    Check to ensure that a number is between two other numbers. Raises
+    AssertionError if the number is not between the other two numbers
+    """
+    assert lower_limit <= actual <= upper_limit, "{0} is not between {1} and {2}".format(actual, lower_limit, upper_limit)
 
 
 # Test to fail the `test_are_equal` function
@@ -44,3 +66,9 @@ def test_is_in(collection, item):
 
 # Test to fail the `test_is_in` function
 # test_is_in([1], 2)
+
+# Test to fail the `test_not_in` function
+# test_not_in([1], 1)
+
+# Test to fail the `test_between` function
+# test_between(10, 1, 200)
