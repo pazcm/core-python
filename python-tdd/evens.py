88a0acdf4b1c4642d2985f0e23a103463957baa8
Marta Paz <pazmarta@gmail.com>
2019-02-08 13:50:56 +0000

    refactor TDD test

--- python-tdd/evens.py
+++ python-tdd/evens.py
@@ -1,20 +1,13 @@
-def even_number_of_evens(numbers):
-    return False
+def is_even(number):
+    """
+    A simple helper function that will check to see if a number is even or not
+    `number` is the number that we wish to check
+    Returns True or False depending on whether or not the number is evenly
+    divisble by 2
+    """
+    return number % 2 == 0
 
-assert even_number_of_evens([]) == False, "No numbers"   
-# assert even_number_of_evens([2]) == False, "One even number"
-# assert even_number_of_evens([2, 4]) == True, "Two even numbers"
-# assert even_number_of_evens([2, 3]) == False, "Two numbers, only one even"
-# assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8]) == False, "Multiple numbers, three even"
-# assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8, 5, 12]) == True, "Multiple numbers, four even"
-# assert even_number_of_evens([1, 3, 9]) == False, "No even numbers"
 
-print("All tests passed!")
-
-"""
-Test2>
-function that accepts a list of numbers and returns True if the list contains an even number of even numbers.
-"""
 def even_number_of_evens(numbers):
     """
     Returns the number of even numbers contained in a list of numbers.
@@ -26,25 +19,11 @@ def even_number_of_evens(numbers):
         - if the number of even numbers is 0, return `False`
         - if the number of even numbers is even, return `True`
     """
-
-    # Check to see if the list is empty
-    if numbers == []:
-        return False
-    else:
-        # Set a `number_of_evens` variable that will be incremented each time
-        # an even number is found
-        evens = 0
         
     # Iterate of over each item and if it's an even number, increment the
     # `evens` variable
-    for number in numbers:
-        if number % 2 == 0:
-            evens += 1
-    
-    if evens == 0:
-        return False
-    else:
-        return evens % 2 == 0
+    evens = sum([1 for n in numbers if is_even(n)])
+    return False if evens == 0 else is_even(evens)
 
 # Our set of test cases
 assert even_number_of_evens([]) == False, "No numbers"
@@ -57,52 +36,4 @@ assert even_number_of_evens([1, 3, 9]) == False, "No even numbers"
 
 # If all the test cases pass, print some successful info to the console to let
 # the developer know
-print("Hi Marta, All tests passed!")
-
-"""
-TDD
-Test3>
-"""
-def even_number_of_evens(numbers):
-    """
-    Returns the number of even numbers contained in a list of numbers.
-    `numbers` should be a list containing numbers
-    
-    Returns either True or False based on a number of criteria.
-        - if `numbers` is empty, return `False`
-        - if the number of even numbers is odd, return `False`
-        - if the number of even numbers is 0, return `False`
-        - if the number od even numbers is even, return `True`
-    """
-
-    # Check to see if the list is empty
-    if numbers == []:
-        return False
-    else:
-        # Set a `number_of_evens` variable that will be incremented each time
-        # an even number is found
-        evens = 0
-        
-    # Iterate of over each item and if it's an even number, increment the
-    # `evens` variable
-    for number in numbers:
-        if number % 2 == 0:
-            evens += 1
-    
-    if evens == 0:
-        return False
-    else:
-        return evens % 2 == 0
-
-# Our set of test cases
-assert even_number_of_evens([]) == False, "No numbers"
-assert even_number_of_evens([2]) == False, "One even number"
-assert even_number_of_evens([2, 4]) == True, "Two even numbers"
-assert even_number_of_evens([2, 3]) == False, "Two numbers, only one even"
-assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8]) == False, "Multiple numbers, three even"
-assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8, 5, 12]) == True, "Multiple numbers, four even"
-assert even_number_of_evens([1, 3, 9]) == False, "No even numbers"
-
-# If all the test cases pass, print some successful info to the console to let
-# the developer know
-print("All tests passed! TDD way")
+print("All tests passed!")
